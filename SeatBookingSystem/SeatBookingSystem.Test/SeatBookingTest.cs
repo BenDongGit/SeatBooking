022ac51c1c0ff3c6936dad96cabb9a3fb88ae9c1
Ben Dong (Wicresoft) <=v-bedong@microsoft.com>
2018-02-05 09:46:31 +0800

    added test project

---
+++ SeatBookingSystem/SeatBookingSystem.Test/SeatBookingTest.cs
@@ -0,0 +1,14 @@
+﻿namespace SeatBookingSystem.Test
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class SeatBookingTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
